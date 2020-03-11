a483d854138fbc6211db84543b2d56b543709479
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-03-11 14:19:39 -0400

    Add a Slack integration

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'net/http'
 require 'sinatra'
 require 'sinatra/cors'
 
@@ -30,15 +31,30 @@ end
 def handle_uri(uri)
   puts "Received: #{uri}"
   parsed = URI(uri)
-  puts "*** WARNING *** Invalid domain: #{uri}" unless valid?(parsed.host)
+
+  unless valid?(parsed.host)
+    error_message = "Received request from invalid domain: #{uri}"
+    puts error_message
+    post_to_slack! error_message
+  end
 end
 
 # development, staging and prod are ok; everything else not so much
 def valid?(host)
   return true if host =~ /\.artsy.net$/
 
+  # TODO: uncomment after this is verified working
   # return true if host == 'localhost'
   # return true if host == '127.0.0.1'
 
   false
+end
+
+def post_to_slack!(message)
+  slack_endpoint = URI(ENV['SLACK_ENDPOINT'])
+  payload = {
+    username: 'Francis Beacon',
+    text: message
+  }
+  Net::HTTP.post slack_endpoint, payload.to_json, 'Content-Type' => 'application/json'
 end
