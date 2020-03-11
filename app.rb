6beed1ccf1361c3f1b2ee9902d8a508c8001c44b
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-03-11 14:19:38 -0400

    Cop

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/cors'
 
@@ -13,7 +15,7 @@ end
 #  GET /a?u=<URI encoded value of window.location.href>
 # e.g. /a?u=http%3A%2F%2Fglitch.me%2Fl33t
 get '/a' do
-  handleURI(params['u'])
+  handle_uri(params['u'])
 end
 
 # POST /b { u: <value of window.location.href> }
@@ -21,21 +23,20 @@ end
 post '/b' do
   body = request.body.read
   data = JSON.parse(body)
-  handleURI(data['u'])
+  handle_uri(data['u'])
 end
 
 # parse the hostname out of the received uri and warn if invalid
-def handleURI(uri)
+def handle_uri(uri)
   puts "Received: #{uri}"
   parsed = URI(uri)
-  unless valid?(parsed.host)
-    puts "*** WARNING *** Invalid domain: #{uri}"
-  end
+  puts "*** WARNING *** Invalid domain: #{uri}" unless valid?(parsed.host)
 end
 
 # development, staging and prod are ok; everything else not so much
 def valid?(host)
   return true if host =~ /\.artsy.net$/
+
   # return true if host == 'localhost'
   # return true if host == '127.0.0.1'
 
